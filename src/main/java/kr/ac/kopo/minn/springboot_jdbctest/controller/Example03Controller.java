21acf9b4be01ea1826997b2167190720dc4cb000
janrendel <janrendel11@gmail.com>
2025-09-15 10:20:08 +0900

    @Query

---
+++ src/main/java/kr/ac/kopo/minn/springboot_jdbctest/controller/Example03Controller.java
@@ -0,0 +1,53 @@
+package kr.ac.kopo.minn.springboot_jdbctest.controller;
+
+import kr.ac.kopo.minn.springboot_jdbctest.domain.Member;
+import kr.ac.kopo.minn.springboot_jdbctest.repository.MemberRepository03;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/exam03")
+public class Example03Controller {
+    @Autowired
+    MemberRepository03 repository;
+
+    @GetMapping
+    public String viewHomePage(Model model) {
+        Iterable<Member> memberList =  repository.selectMethod();
+        model.addAttribute("memberList", memberList);
+        return "viewPage03";
+    }
+
+    @GetMapping("/new")
+    public String newMethod(Model model) {
+        model.addAttribute("member", new Member());
+        return "viewPage03_new";
+    }
+
+    @PostMapping("/insert")
+    public String insertMethod(@ModelAttribute("member")Member member) {
+        repository.insertMethod(member.getName(), member.getAge(), member.getEmail());
+        return "redirect:/exam03";
+    }
+
+    @GetMapping("edit/{id}")
+    public String editMethod(Model model, @PathVariable(name = "id")int id) {
+        Member member = repository.selectMethodById(id);
+        model.addAttribute("memberList", member);
+        return "viewPage03_edit";
+    }
+
+    @PostMapping("/upadate")
+    public String upadate(@ModelAttribute("member")Member member) {
+        repository.updateMethod(member.getName(), member.getAge(), member.getEmail(), member.getId());
+        return "redirect:/exam03";
+    }
+
+    @GetMapping("/delete{id}")
+    public String deleteMethod(@PathVariable(name = "id")int id) {
+        repository.deleteMethod(id);
+        return "redirect:/exam03";
+    }
+}
---
+++ src/main/java/kr/ac/kopo/minn/springboot_jdbctest/controller/JDBCController.java
@@ -0,0 +1,65 @@
+package kr.ac.kopo.minn.springboot_jdbctest.controller;
+
+import kr.ac.kopo.minn.springboot_jdbctest.domain.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/exam01")
+public class JDBCController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @GetMapping
+    public String requestMethod(Model model) {
+        String sql = "select * from person";
+        List<Person> personList = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Person.class));
+        model.addAttribute("personList", personList);
+        return "viewPage01";
+    }
+
+    @GetMapping("/new")
+    public String newMethod(Model model) {
+        Person person = new Person();
+        model.addAttribute("person", person);
+        return "viewPage01_new";
+    }
+
+    @PostMapping("/insert")
+    public String insertMethod(@ModelAttribute Person person) {
+        String sql = "insert into person (name, age, email) values (?, ?, ?)";
+//        jdbcTemplate.update(sql, person.getName(), person.getAge(), person.getEmail());
+        Object[] params = {person.getName(), person.getAge(), person.getEmail()};
+        int resultCount = jdbcTemplate.update(sql, params);
+        return "redirect:/exam01";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editMethod(@PathVariable int id, Model model) {
+        String sql = "select * from person where id = ?";
+        Person person = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Person.class), id);
+        model.addAttribute("person", person);
+        return "viewPage01_edit";
+    }
+
+    @PostMapping("/update")
+    public String updateMethod(@ModelAttribute("Person") Person person) {
+        String sql = "update person set name = ?, age = ?, email = ? where id = ?";
+        Object[] params = {person.getName(), person.getAge(), person.getEmail(), person.getId()};
+        int resultCount = jdbcTemplate.update(sql, params);
+        return "redirect:/exam01";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMethod(@PathVariable(name="id") int id) {
+        String sql = "delete from person where id = ?";
+        int resultCount = jdbcTemplate.update(sql, id);
+        return "redirect:/exam01";
+    }
+}
